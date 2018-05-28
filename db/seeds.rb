2b923f0fe9e737c3653438eedad7d829d14b7fe8
Nina Yujiri <ninayujiri@gmail.com>
2018-05-28 22:25:43 +0000

    changed image urls in seed file"

--- db/seeds.rb
+++ db/seeds.rb
@@ -51,7 +51,7 @@ Product.destroy_all
 cat1.products.create!({
   name:  'Men\'s Classy shirt',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('http://res.cloudinary.com/ninayujiri/image/upload/v1527545444/apparel1.jpg'),
+  image: 'http://res.cloudinary.com/ninayujiri/image/upload/v1527545444/apparel1.jpg',
   quantity: 10,
   price: 64.99
 })
@@ -59,7 +59,7 @@ cat1.products.create!({
 cat1.products.create!({
   name:  'Women\'s Zebra pants',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('http://res.cloudinary.com/ninayujiri/image/upload/v1527545444/apparel2.jpg'),
+  image: 'http://res.cloudinary.com/ninayujiri/image/upload/v1527545444/apparel2.jpg',
   quantity: 18,
   price: 124.99
 })
@@ -67,7 +67,7 @@ cat1.products.create!({
 cat1.products.create!({
   name:  'Hipster Hat',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('http://res.cloudinary.com/ninayujiri/image/upload/v1527545444/apparel3.jpg'),
+  image: 'http://res.cloudinary.com/ninayujiri/image/upload/v1527545444/apparel3.jpg',
   quantity: 4,
   price: 34.49
 })
@@ -75,7 +75,7 @@ cat1.products.create!({
 cat1.products.create!({
   name:  'Hipster Socks',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('http://res.cloudinary.com/ninayujiri/image/upload/v1527545444/apparel4.jpg'),
+  image: 'http://res.cloudinary.com/ninayujiri/image/upload/v1527545444/apparel4.jpg',
   quantity: 8,
   price: 25.00
 })
@@ -83,7 +83,7 @@ cat1.products.create!({
 cat1.products.create!({
   name:  'Russian Spy Shoes',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('http://res.cloudinary.com/ninayujiri/image/upload/v1527545444/apparel5.jpg'),
+  image: 'http://res.cloudinary.com/ninayujiri/image/upload/v1527545444/apparel5.jpg',
   quantity: 8,
   price: 1_225.00
 })
@@ -91,7 +91,7 @@ cat1.products.create!({
 cat1.products.create!({
   name:  'Human Feet Shoes',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('http://res.cloudinary.com/ninayujiri/image/upload/v1527545444/apparel6.jpg'),
+  image: 'http://res.cloudinary.com/ninayujiri/image/upload/v1527545444/apparel6.jpg',
   quantity: 82,
   price: 224.50
 })
@@ -100,7 +100,7 @@ cat1.products.create!({
 cat2.products.create!({
   name:  'Modern Skateboards',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('http://res.cloudinary.com/ninayujiri/image/upload/v1527545444/apparel7.jpg'),
+  image: 'http://res.cloudinary.com/ninayujiri/image/upload/v1527545444/apparel7.jpg',
   quantity: 40,
   price: 164.49
 })
@@ -108,7 +108,7 @@ cat2.products.create!({
 cat2.products.create!({
   name:  'Hotdog Slicer',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('http://res.cloudinary.com/ninayujiri/image/upload/v1527545444/apparel8.jpg'),
+  image: 'http://res.cloudinary.com/ninayujiri/image/upload/v1527545444/apparel8.jpg',
   quantity: 0,
   price: 26.00
 })
@@ -116,7 +116,7 @@ cat2.products.create!({
 cat2.products.create!({
   name:  'World\'s Largest Smartwatch',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('http://res.cloudinary.com/ninayujiri/image/upload/v1527545444/electronics3.jpg'),
+  image: 'http://res.cloudinary.com/ninayujiri/image/upload/v1527545444/electronics3.jpg',
   quantity: 32,
   price: 2_026.29
 })
@@ -124,7 +124,7 @@ cat2.products.create!({
 cat3.products.create!({
   name:  'Optimal Sleeping Bed',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('http://res.cloudinary.com/ninayujiri/image/upload/v1527545444/furniture1.jpg'),
+  image: 'http://res.cloudinary.com/ninayujiri/image/upload/v1527545444/furniture1.jpg',
   quantity: 320,
   price: 3_052.00
 })
@@ -132,7 +132,7 @@ cat3.products.create!({
 cat3.products.create!({
   name:  'Electric Chair',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('http://res.cloudinary.com/ninayujiri/image/upload/v1527545444/furniture2.jpg'),
+  image: 'http://res.cloudinary.com/ninayujiri/image/upload/v1527545444/furniture2.jpg',
   quantity: 2,
   price: 987.65
 })
@@ -140,7 +140,7 @@ cat3.products.create!({
 cat3.products.create!({
   name:  'Red Bookshelf',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('http://res.cloudinary.com/ninayujiri/image/upload/v1527545444/furniture3.jpg'),
+  image: 'http://res.cloudinary.com/ninayujiri/image/upload/v1527545444/furniture3.jpg',
   quantity: 23,
   price: 2_483.75
 })
