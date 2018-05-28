cab5bfe47be32a9ec19e7852f22fcbd2caf11483
Nina Yujiri <ninayujiri@gmail.com>
2018-05-28 22:38:03 +0000

    edited seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,8 @@ User.destroy_all
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     email: Faker::Internet.email,
-    password_digest: Faker::Internet.password
+    password: password123,
+    password_confirmation: password123
   })
 end
 
