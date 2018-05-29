d8e78c2f3c12ebfbf1da1fdaf328fa75bcf09606
Nina Yujiri <ninayujiri@gmail.com>
2018-05-29 18:12:08 +0000

    finished product details feature specs

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to the product details page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: 'apparel1.jpg',
+        quantity: 10,
+        price: 64.99
+      )
+  end
+
+  scenario "They see the product they clicked on" do
+    # ACT
+    visit root_path
+    click_on 'Details'
+    visit '/products/1'
+
+    # DEBUG
+    save_screenshot
+
+    # VERIFY
+    expect(page).to have_css 'section.products-show'
+  end
+end
